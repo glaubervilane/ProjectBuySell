60f655f95c897843bb2164a764ef2d1a7b829673
Glauber Marques <glauberfmarques@gmail.com>
2023-07-30 19:03:53 -0600

    set server side application and initialize the first view

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-/* eslint-disable no-console */
-require('dotenv').config();
+// server/server.js
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// Set the 'views' directory correctly to point to the 'server/views' folder
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
-
-// Add any additional middleware you may need
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mounting Routes
 const authRoutes = require('./routes/authRoutes');
@@ -23,9 +25,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
 
-// Home page
+// Home page route
 app.get('/', (req, res) => {
-  res.render('index');
+  // Example data for featured items (you can replace this with real data from your database)
+  const featuredItems = [
+    { name: 'Item 1', price: 19.99 },
+    { name: 'Item 2', price: 29.99 },
+    // Add more items here...
+  ];
+
+  res.render('index', { items: featuredItems });
 });
 
 // Start the server
